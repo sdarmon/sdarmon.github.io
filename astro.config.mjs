fdcaa7e6ab8a4a7c28de94da093e3b642ea28c88
sdarmon <sasha.darmon@univ-lyon1.fr>
2026-04-05 19:11:56 +0200

    Update : Ajout du sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap'; // AJOUT
 
 export default defineConfig({
     site: 'https://sashadarmon.fr',
-    base: '/',
+    integrations: [sitemap()], // AJOUT
 });
